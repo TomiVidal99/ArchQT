12203b37cf171b2c67711db97d13f12c2d198adf
Tomas Vidal <tomi_vidal_99@hotmail.com>
2021-11-02 13:12:31 -0300

    fixed typo

--- main.sh
+++ main.sh
@@ -26,7 +26,7 @@
     echo -t "\n   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝ ╚══▀▀═╝    ╚═╝    \n" 
     echo -t "\n------------------------------------------------------------------------"
 
-    echo -t "format disk on the main its: $(format_disk)\n"
+    echo -t "format disk on the main its: $format_disk\n"
 
     echo $(bash 0-preinstall.sh $region $format_disk $disk)
     echo $(arch-chroot /mnt /root/ArchQT/1-setup.sh $region $timezone $locale $keymap $username)
