67ff77a16fbd5c963752acd973347d195c6cb48d
Tomas Vidal <tomi_vidal_99@hotmail.com>
2021-11-02 15:19:51 -0300

    fixed typo

--- main.sh
+++ main.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-    username=$1
-    username_password=$2
-    root_password=$3
-    hostname=$4
-    desktop_environment=$5
-    custom_locale=$6
-    locale=$7
-    custom_keymap=$8
-    keymap=$9
-    custom_region=${10}
-    region=${11}
-    timezone=${12}
-    nvim_config=${13}
-    format_disk=${14}
-    disk=${15}
+    args=("$@")
+    username=${args[0]}
+    username_password=${args[1]}
+    root_password=${args[2]}
+    hostname=${args[3]}
+    desktop_environment=${args[4]}
+    custom_locale=${args[5]}
+    locale=${args[6]}
+    custom_keymap=${args[7]}
+    keymap=${args[8]}
+    custom_region=${args[9]}
+    region=${args[10]}
+    timezone=${args[11]}
+    nvim_config=${args[12]}
+    format_disk=${args[13]}
+    disk=${args[14]}
 
     echo -t "\n------------------------------------------------------------------------\n"
     echo -t "\n    █████╗ ██████╗  ██████╗██╗  ██╗ ██████╗ ████████╗ \n" 
@@ -24,8 +25,6 @@
     echo -t "\n   ██║  ██║██║  ██║╚██████╗██║  ██║╚██████╔╝   ██║    \n" 
     echo -t "\n   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝ ╚══▀▀═╝    ╚═╝    \n" 
     echo -t "\n------------------------------------------------------------------------"
-
-    echo -t "format disk on the main its: $format_disk\n"
 
     echo $(bash 0-preinstall.sh $region $format_disk $disk)
     echo $(arch-chroot /mnt /root/ArchQT/1-setup.sh $region $timezone $locale $keymap $username)
