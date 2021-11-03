d0acc797b5cce554df4b5411114bcd93331478a4
Tomas Vidal <tomi_vidal_99@hotmail.com>
2021-11-03 16:31:22 -0300

    addded install nvim config

---
+++ nvim-config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+github_repository="https:///github.com/TomiVidal99/nvim_config.git"
+
+# create the paths
+mkdir -p ~/.config/nvim
+cd ~/.config/nvim
+
+#  clone the config repo
+git clone $github_repository .
+
+# install neovim Plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+nvim +PlugInstall
